46f8dcffd229f6cd061f52943b32f385aab06841
Derek <112670195+dsmdex@users.noreply.github.com>
2025-06-08 17:13:14 -0700

    note removal

--- session_two_set_one.py
+++ session_two_set_one.py
@@ -1,26 +1,3 @@
-# Note Taking:
-# The Umpire Strategy
-# Understand, Match, Plan, Implement, Review, Evaluate
-    # Understand the Problem
-        #   Understand what the interviewer is asking with
-        #       clarifying questions
-        #   Generate basic sample input and output
-        #   Explore more unusual edge cases
-        #   Explore tradeoffs (memory, performance, code 
-        #       simplicity/refactoring)
-        #   * What is the end goal or vision of the problem.
-    # Plan the Solution
-        #   Describe your overall approach in one or two sentences
-        #   Write down the steps in English
-        #   Each step should be simple and clear
-    # Implement the Solution
-        #   Translate your plan into code
-        #       - A clear plan should be relativelu easy to 
-        #           translate. If it's hard to translate, 
-        #           the plan probably isn't good enough.
-        #   Look up any basic Python that you need.
-
-
 # Problem Set One
 
 # Problem 1: Print List
